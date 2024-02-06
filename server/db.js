da086551aa9a9973dd315aac9a93ba611c6b8078
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2024-02-06 10:52:55 +0530

    First Commit

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const Pool = require('pg').Pool;
+
+
+const pool = new Pool({
+    user: 'postgres',
+    password:'123456',
+    host:'localhost',
+    port:'5432',
+    database:'perntodo'
+});
+
+module.exports = pool
